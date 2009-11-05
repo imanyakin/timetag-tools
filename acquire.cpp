55a7f4767d8e0bd8077ad51b1583e3df9c2565f9
Ben Gamari <bgamari.foss@gmail.com>
2009-11-05 18:30:15 -0500

    Bring acquire script up-to-date with hardware

--- acquire.cpp
+++ acquire.cpp
@@ -18,34 +18,20 @@ struct buffer {
 };
 
 #define TIMEOUT 100
+#define DATA_TIMEOUT 5000
 
 
 struct buffer* buffers;
 
-void configure_pulse_sequencer(libusb_device_handle* dev, char seq_mask, bool initial_state, int initial_count, int high_count, int low_count) {
-	int ret, transferred;
-	uint8_t* buffer = (uint8_t*) calloc(sizeof(uint8_t), 128);
-
-	buffer[0] = seq_mask;
-	buffer[1] = initial_state;
-	int* tmp = (int*) &buffer[2];
-	tmp[0] = initial_count;
-	tmp[1] = high_count;
-	tmp[2] = low_count;
-
-	if (ret = libusb_bulk_transfer(dev, CMD_ENDP, buffer, 128, &transferred, TIMEOUT) )
-		fprintf(stderr, "Failed sending pulse sequencer configuration request: %d\n", ret);
-	if (ret = libusb_bulk_transfer(dev, REPLY_ENDP, buffer, 128, &transferred, TIMEOUT) )
-		fprintf(stderr, "Failed receiving reply: %d\n", ret);
-
-	if (transferred >= 2)
-		fprintf(stderr, "Length: %hu\n", ((uint16_t*) buffer)[0]);
-}
-
-static void send_simple_command(libusb_device_handle* dev, char cmd) {
+static void send_simple_command(libusb_device_handle* dev, uint8_t mask, uint8_t* data, int len) {
 	int ret, transferred;
 	uint8_t* buffer = (uint8_t*) calloc(sizeof(uint8_t), 1024);
-	buffer[0] = cmd;
+
+	buffer[0] = len;
+	buffer[1] = mask;
+	for (int i=0; i<len; i++)
+		buffer[i+2] = data[i];
+
 	if (ret = libusb_bulk_transfer(dev, CMD_ENDP, buffer, 128, &transferred, TIMEOUT) )
 		fprintf(stderr, "Failed sending request: %d\n", ret);
 	if (ret = libusb_bulk_transfer(dev, REPLY_ENDP, buffer, 128, &transferred, TIMEOUT) )
@@ -53,36 +39,67 @@ static void send_simple_command(libusb_device_handle* dev, char cmd) {
 
 	if (transferred > 4)
 		fprintf(stderr, "Length: %d\n", ((int*) buffer)[0]);
+	else
+		fprintf(stderr, "No response\n");
+}
+
+void configure_pulse_sequencer(libusb_device_handle* dev, char seq_mask,
+		bool initial_state, int initial_count, int high_count, int low_count) {
+	int ret, transferred;
+	uint8_t buffer[1024];
+
+	// Set initial state
+	buffer[0] = 0x00;
+	buffer[1] = 0x00;
+	buffer[2] = 0x00;
+	buffer[3] = initial_state;
+	buffer[4] = 0x01;
+	send_simple_command(dev, 0x4, buffer, 5);
+
+	// Set initial count
+	buffer[0] = 0xff & (initial_count >> 24);
+	buffer[1] = 0xff & (initial_count >> 16);
+	buffer[2] = 0xff & (initial_count >> 8);
+	buffer[3] = 0xff & (initial_count >> 0);
+	buffer[4] = 0x02;
+	send_simple_command(dev, 0x4, buffer, 5);
+
+	// Set high count
+	buffer[0] = 0xff & (high_count >> 24);
+	buffer[1] = 0xff & (high_count >> 16);
+	buffer[2] = 0xff & (high_count >> 8);
+	buffer[3] = 0xff & (high_count >> 0);
+	buffer[4] = 0x04;
+	send_simple_command(dev, 0x4, buffer, 5);
+
+	// Set low count
+	buffer[0] = 0xff & (low_count >> 24);
+	buffer[1] = 0xff & (low_count >> 16);
+	buffer[2] = 0xff & (low_count >> 8);
+	buffer[3] = 0xff & (low_count >> 0);
+	buffer[4] = 0x04;
+	send_simple_command(dev, 0x4, buffer, 5);
+
+	if (ret = libusb_bulk_transfer(dev, REPLY_ENDP, buffer, 128, &transferred, TIMEOUT) )
+		fprintf(stderr, "Failed receiving reply: %d\n", ret);
+
+	if (transferred >= 2)
+		fprintf(stderr, "Length: %hu\n", ((uint16_t*) buffer)[0]);
 }
 
 void start_capture(libusb_device_handle* dev) {
-	send_simple_command(dev, 0x4);
+	uint8_t buffer[1] = { 0x1 };
+	send_simple_command(dev, 0x1, buffer, 1);
 }
 
 void stop_capture(libusb_device_handle* dev) {
-	send_simple_command(dev, 0x2);
+	uint8_t buffer[1] = { 0x2 };
+	send_simple_command(dev, 0x1, buffer, 1);
 }
 
 void reset_counter(libusb_device_handle* dev) {
-	send_simple_command(dev, 0x1);
-}
-
-static void data_ready_cb(struct libusb_transfer* transfer) {
-	struct buffer* buf = (struct buffer*) transfer->user_data;
-	for (int i=0; i<transfer->actual_length; i++)
-		printf("%c ", transfer->buffer[i]);
-	libusb_free_transfer(transfer);
-	free(buf);
-}
-
-void read_data(libusb_device_handle* dev) {
-	const int buf_sz = 1024;
-
-	struct buffer* buf = (struct buffer*) malloc(sizeof(struct buffer) + buf_sz);
-	buf->size = buf_sz;
-	buf->transfer = libusb_alloc_transfer(0);
-	libusb_fill_bulk_transfer(buf->transfer, dev, DATA_ENDP, &buf->data[0], buf_sz, data_ready_cb, buf, 0);
-	libusb_submit_transfer(buf->transfer);
+	uint8_t buffer[1] = { 0x4 };
+	send_simple_command(dev, 0x1, buffer, 1);
 }
 
 void get_status(libusb_device_handle* dev) {
@@ -97,29 +114,16 @@ void get_status(libusb_device_handle* dev) {
 	printf("\n");
 }
 
-void test2(libusb_device_handle* dev) {
+void read_loop(libusb_device_handle* dev) {
 	int ret, transferred;
 	const int N = 85;
-	uint8_t buffer[512];
+	uint8_t buffer[N*6];
 
-	if (ret = libusb_bulk_transfer(dev, 0x86, &buffer[0], 512, &transferred, TIMEOUT) )
-		fprintf(stderr, "Failed sending request: %d\n", ret);
-#if 0
-	printf("Transferred: %d:\n", transferred);
-        for (int i=0; i<transferred; i++)
-		printf("%02x ", buffer[i]);
-	printf("\n");
-#else
-	for (int i=0; i<transferred/6; i++) {
-		uint64_t a = *((uint64_t*) &buffer[i*6]);
-		printf("  t=%lld\t%d  %d  %d  %d\n",
-				a & 0x0FffffFFFFL,
-				!!(a & (1L << 44)),
-				!!(a & (1L << 45)),
-				!!(a & (1L << 46)),
-				!!(a & (1L << 47)));
+	while (1) {
+		if (ret = libusb_bulk_transfer(dev, 0x86, &buffer[0], N*6, &transferred, DATA_TIMEOUT) )
+			fprintf(stderr, "Failed sending request: %d\n", ret);
+		fwrite(buffer, 1, transferred, stdout);
 	}
-#endif
 }
 
 int main(int argc, char** argv) {
@@ -136,19 +140,19 @@ int main(int argc, char** argv) {
 
 
 	get_status(dev);
-	stop_capture(dev);
-	//for (int i=0; i<3; i++)
-		//test2(dev);
-	start_capture(dev);
-	for (int i=0; i<3; i++)
-		test2(dev);
 	//stop_capture(dev);
-	//for (int i=0; i<3; i++)
-		//test2(dev);
+	//for (int i=0; i<3; i++) test2(dev);
+
+	start_capture(dev);
+	//for (int i=0; i>-1; i++) test2(dev);
+
+	stop_capture(dev);
+	//for (int i=0; i<3; i++) test2(dev);
+
 	configure_pulse_sequencer(dev, 0x70, true, 10000, 20000, 30000);
 	fprintf(stderr, "Done\n");
 
-	//read_data(dev);
+	read_loop(dev);
 
 	libusb_release_interface(dev, 0);
 	libusb_close(dev);
