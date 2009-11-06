fd4ca4c122ec2aaafee65918dcd3714d365eeeaa
Ben Gamari <bgamari.foss@gmail.com>
2009-11-05 21:10:36 -0500

    Fix dump_photons

--- dump_photons.cpp
+++ dump_photons.cpp
@@ -22,19 +22,20 @@
 int main(int argc, char** argv) {
 	unsigned int count = 0;
 	while (true) {
+		record_t photon = 0;
 		count++;
 
-		record_t photon;
 		if (fread(&photon, RECORD_LENGTH, 1, stdin) != 1)
 			exit(!feof(stdin));
-		count_t time = be64toh(photon & TIME_MASK);
+		photon = be64toh(photon) >> 16;
+		count_t time = photon & TIME_MASK;
 
-		printf("%u\t%llu\t%s %s %s %s\n",
+		printf("%u\t%llx\t%s %s %s %s\n",
 				count, time,
-				photon & CHAN_1_MASK ? "1" : "",
-				photon & CHAN_2_MASK ? "2" : "",
-				photon & CHAN_3_MASK ? "3" : "",
-				photon & CHAN_4_MASK ? "4" : "");
+				photon & CHAN_1_MASK ? "1" : " ",
+				photon & CHAN_2_MASK ? "2" : " ",
+				photon & CHAN_3_MASK ? "3" : " ",
+				photon & CHAN_4_MASK ? "4" : " ");
 	}
 
 	return 0;
