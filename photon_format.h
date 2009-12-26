bc5c2e6dc8ff36c8f22c617db3e23ebb98db180d
Ben Gamari <bgamari.foss@gmail.com>
2009-12-26 04:01:14 -0500

    Bring photon record format up-to-date
    Wire format changed to accomodate laser transition timestamping

--- photon_format.h
+++ photon_format.h
@@ -8,18 +8,15 @@ typedef unsigned long long count_t; // Represents a time counter value
 #define TIME_BITS 36
 #define TIME_MASK ((1ULL<<TIME_BITS) - 1)
 
-#define CHAN_1_MASK (1ULL << 37)
-#define CHAN_2_MASK (1ULL << 38)
-#define CHAN_3_MASK (1ULL << 39)
-#define CHAN_4_MASK (1ULL << 40)
+#define CHAN_1_MASK (1ULL << 36)
+#define CHAN_2_MASK (1ULL << 37)
+#define CHAN_3_MASK (1ULL << 38)
+#define CHAN_4_MASK (1ULL << 39)
 #define CHANNEL_MASK (CHAN_1_MASK | CHAN_2_MASK | CHAN_3_MASK | CHAN_4_MASK)
 
-#define LOST_SAMPLE_MASK (1ULL << 41)
-
-#define OUTPUT_1_MASK (1ULL << 44)
-#define OUTPUT_2_MASK (1ULL << 45)
-#define OUTPUT_3_MASK (1ULL << 46)
-#define OUTPUT_4_MASK (1ULL << 47)
+#define REC_TYPE_MASK (1ULL << 45)
+#define TIMER_WRAP_MASK (1ULL << 46)
+#define LOST_SAMPLE_MASK (1ULL << 47)
 
 #endif
 
