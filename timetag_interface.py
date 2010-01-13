43ab895450b5dc289f9cb6a3f99717d2e2c76d63
Ben Gamari <bgamari.foss@gmail.com>
2010-01-13 14:53:56 -0500

    Add timetag interface for UI

---
+++ timetag_interface.py
@@ -0,0 +1,22 @@
+class TimetagInterface(object):
+        def __init__(self, file):
+                self.fd = file
+                
+        def set_initial_state(self, mask, state):
+                self.fd.write("set_initial_state %d %d\n" % (mask, state))
+
+        def set_initial_count(self, mask, count):
+                self.fd.write("set_initial_count %d %d\n" % (mask, count))
+
+        def set_high_count(self, mask, count):
+                self.fd.write("set_high_count %d %d\n" % (mask, count))
+
+        def set_low_count(self, mask, count):
+                self.fd.write("set_low_count %d %d\n" % (mask, count))
+
+        def start_capture(self):
+                self.fd.write("start_capture\n")
+
+        def stop_capture(self):
+                self.fd.write("stop_capture\n")
+
