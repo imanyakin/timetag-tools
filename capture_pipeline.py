277cdedc5a8a8bd0cea11558cc1ede8091bf76c3
Ben Gamari <bgamari@blinky.(none)>
2010-01-21 16:06:49 -0500

    Move ringbuffer into separate file

--- capture_pipeline.py
+++ capture_pipeline.py
@@ -6,7 +6,7 @@ from time import sleep, time
 import os
 import threading
 from collections import defaultdict
-from array import array
+from ringbuffer import RingBuffer
 
 import timetag_interface
 
@@ -14,34 +14,6 @@ logging.basicConfig(level=logging.DEBUG)
 
 CAPTURE_CLOCK=30e6 # Hz
 bin_root = "/usr/bin"
-
-class RingBuffer:
-	def __init__(self, length):
-		self._cur = 0
-		self._size = length
-		self._data = array.array('f', [0] * length)
-
-	def append(self, x):
-		"""append an element at the end of the buffer"""
-		self._data[self._cur] = x
-                self._cur += 1
-		if self._cur == self._size:
-			self._cur = 0
-			self.__class__ = RingBuffer.RingBufferFull
-
-	def get(self):
-  		""" return a list of elements from the oldest to the newest"""
-		return self._data
-
-
-        class RingBufferFull:
-                def append(self, x):
-                        self._data[self._cur] = x
-                        self._cur = int((self._cur+1) % self._size)
-
-                def get(self):
-                        return self.data[self._cur:] + self.data[:self._cur]
-		
 
 class CapturePipeline(object):
         class Channel(object):
---
+++ ringbuffer.py
@@ -0,0 +1,29 @@
+from array import array
+
+class RingBuffer:
+	def __init__(self, length, fmt='f'):
+		self._cur = 0
+		self._size = length
+		self._data = array.array(fmt, [0] * length)
+
+	def append(self, x):
+		"""append an element at the end of the buffer"""
+		self._data[self._cur] = x
+                self._cur += 1
+		if self._cur == self._size:
+			self._cur = 0
+			self.__class__ = RingBuffer.RingBufferFull
+
+	def get(self):
+  		""" return a list of elements from the oldest to the newest"""
+		return self._data
+
+
+        class RingBufferFull:
+                def append(self, x):
+                        self._data[self._cur] = x
+                        self._cur = int((self._cur+1) % self._size)
+
+                def get(self):
+                        return self.data[self._cur:] + self.data[:self._cur]
+
