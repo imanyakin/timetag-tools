70fbc02addb85c058a6835895e602ee0c7e0ecea
Ben Gamari <bgamari.foss@gmail.com>
2010-04-08 09:50:38 -0400

    pytimetag: Fix a few interface issues

--- pytimetag.cpp
+++ pytimetag.cpp
@@ -6,8 +6,16 @@ void eof_translator(end_stream const& x) {
 	PyErr_Format(PyExc_EOFError, "End of stream");
 }
 
+bool get_channel(record r, unsigned int i) {
+	return r.get_channels()[i];
+}
+
 BOOST_PYTHON_MODULE(pytimetag) {
 	register_exception_translator<end_stream>(eof_translator);
+	enum_<record::type>("record_type")
+		.value("STROBE", record::type::STROBE)
+		.value("DELTA", record::type::DELTA);
+
 	class_<record_stream>("Stream", init<int>())
 		.def("get_record", &record_stream::get_record);
 
@@ -16,6 +24,6 @@ BOOST_PYTHON_MODULE(pytimetag) {
 		.add_property("type", &record::get_type)
 		.add_property("wrap_flag", &record::get_wrap_flag)
 		.add_property("lost_flag", &record::get_lost_flag)
-		.add_property("channels", &record::get_channels);
+		.def("get_channel", &get_channel);
 }
 
