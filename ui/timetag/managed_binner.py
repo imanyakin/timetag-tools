1a5ff18682971e4eea978d5557ddce29caf74e05
Ben Gamari <bgamari.foss@gmail.com>
2012-10-23 13:50:53 -0400

    managed_binner: Initial commit
    This infrastructure will hopefully make it easier to implement things
    that require a binner which is started and stopped with the capture
    pipeline

---
+++ ui/timetag/managed_binner.py
@@ -0,0 +1,42 @@
+import gobject
+
+class ManagedBinner(object):
+    POLL_PERIOD = 2
+    def __init__(self, pipeline, name='managed_binner'):
+        self._pipeline = pipeline
+        self._binner = None
+        self._binner_name = '%s:%x' % (name,id(self))
+        self._watch()
+        gobject.timeout_add_seconds(ManagedBinner.POLL_PERIOD, self._watch)
+    
+    def _watch(self):
+        if self._pipeline.is_capture_running() and self._binner is None:
+            self._start_binner()
+        elif not self._pipeline.is_capture_running() and self._binner is not None:
+            self._stop_binner()
+        return True
+
+    def _start_binner(self):
+        assert(self._binner is None)
+        self._binner = self.create_binner()
+        self._pipeline.add_output(self._binner_name, self._binner.get_data_fd())
+        self.on_started()
+
+    def _stop_binner(self):
+        assert(self._binner is not None)
+        self._pipeline.remove_output(self._binner_name)
+        self._binner.stop()
+        self._binner = None
+        self.on_stopped()
+
+    def get_binner(self):
+        return self._binner
+
+    def create_binner(self):
+        raise NotImplemented("ManagedBinner is an abstract class")
+
+    def is_running(self):
+        return self._binner is not None
+
+    def on_started(self): pass
+    def on_stopped(self): pass
