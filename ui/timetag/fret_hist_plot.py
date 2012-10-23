09b13b16b13ed9c3b646a2ec3189587fc0bcffd2
Ben Gamari <bgamari.foss@gmail.com>
2012-10-23 15:18:13 -0400

    fret_hist_plot: Port to managed_binner

--- ui/timetag/fret_hist_plot.py
+++ ui/timetag/fret_hist_plot.py
@@ -10,22 +10,20 @@ from matplotlib.backends.backend_gtkcairo import FigureCanvasGTKCairo
 from collections import defaultdict
 
 from timetag.binner import FretHistBinner
+from timetag.managed_binner import ManagedBinner
 
-class FretHistPlot(object):
+class FretHistPlot(ManagedBinner):
         FigureCanvas = FigureCanvasGTK
 
         def __init__(self, pipeline):
+                self.pipeline = pipeline
                 self.builder = gtk.Builder()
                 src = pkgutil.get_data('timetag', 'fret_hist.glade')
                 self.builder.add_from_string(src)
                 self.builder.connect_signals(self)
                 self.win = self.builder.get_object('hist_window')
                 self.win.connect('destroy', self.destroy_cb)
-                self.pipeline = pipeline
-                self.update_rate = 0.5 # Hz
-
-                self.binner = None
-                self._restart_binner()
+                self.update_rate = 0.3 # Hz
 
                 self.figure = Figure()
                 self.axes = self.figure.add_subplot(111)
@@ -34,59 +32,46 @@ class FretHistPlot(object):
 
                 canvas = self.__class__.FigureCanvas(self.figure)
                 self.builder.get_object('plot_container').pack_start(canvas)
-                self.builder.get_object('nbins').value = 40
                 self.win.show_all()
+                ManagedBinner.__init__(self, self.pipeline, "fret-hist-plot")
 
-                def update_plot():
-                        try:
-                                self.update()
-                        except AttributeError as e:
-                                # Ignore exceptions in case pipeline is shut down
-                                raise e
-                        return self.running
-
-                self.running = True
-                gobject.timeout_add(int(1000.0 / self.update_rate), update_plot)
+        def create_binner(self):
+                get_obj = self.builder.get_object
+                model = get_obj('channel_model')
+                binner = FretHistBinner(self.bin_time, self.pipeline.clockrate)
+                binner.donor_channel = model[get_obj('donor_combo').get_active_iter()][0]
+                binner.acceptor_channel = model[get_obj('acceptor_combo').get_active_iter()][0]
+                binner.threshold = get_obj('threshold').get_value()
+                binner.hist_width = 1. / get_obj('nbins').get_value()
+                return binner
+                
+        def on_started(self):
+                gobject.timeout_add(int(1000.0 / self.update_rate), self._update_plot)
 
         @property
         def bin_time(self):
                 return 1e-3 * self.builder.get_object('bin_time').props.value
 
         def destroy_cb(self, a):
-                self.running = False
-                self._stop_binner()
+                self.stop_binner()
 
-        def _stop_binner(self):
-                if self.binner is not None:
-                        self.pipeline.remove_output(self.output_id)
-                        self.binner.stop()
-
-        def _restart_binner(self):
-                self._stop_binner()
-                self.binner = FretHistBinner(self.bin_time, self.pipeline.clockrate)
-                self.output_id = str(id(self))
-                self.pipeline.add_output(self.output_id, self.binner.get_data_fd())
-
-        def update(self):
-                hist_width = self.binner.hist_width
-                hist = self.binner.hist
-                if len(hist) == 0: return
+        def _update_plot(self):
+                binner = self.get_binner()
+                if binner is None: return False
+                hist_width = binner.hist_width
+                hist = binner.hist
+                if len(hist) == 0: return True
                 self.axes.cla()
                 self.axes.bar(hist.keys(), hist.values(), hist_width)
                 self.axes.relim()
                 self.axes.set_xlim(0, 1)
                 self.axes.set_xlabel('FRET efficiency')
                 self.figure.canvas.draw()
+                return True
 
         def binning_config_changed_cb(self, *args):
-                get_obj = self.builder.get_object
-                model = get_obj('channel_model')
-                self.binner.donor_channel = model[get_obj('donor_combo').get_active_iter()][0]
-                self.binner.acceptor_channel = model[get_obj('acceptor_combo').get_active_iter()][0]
-                self.binner.threshold = get_obj('threshold').get_value()
-                self.binner.hist_width = 1. / get_obj('nbins').get_value()
-                self.binner.reset_hist()
+                self.restart_binner()
 
         def bin_time_changed_cb(self, adj):
-                self._restart_binner()
+                self.restart_binner()
 
